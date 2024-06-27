4482feb6260a6510d8946b2ca92eaee3326ecc21
Júlia Lima <juliafclima@hotmail.com>
2024-06-27 15:50:39 -0300

    Componente ScreenHeader

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,37 +1,64 @@
-import { FlatList, HStack, VStack } from "native-base";
+import { FlatList, HStack, Heading, Text, VStack } from "native-base";
 
+import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { useState } from "react";
 
 export function Home() {
-  const [groupSelected, setGroupSelected] = useState("Costas");
   const [groups] = useState(["Costas", "Bíceps", "Tríceps", "ombro"]);
+  const [exercises] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
 
   return (
     <VStack flex={1}>
-      <HomeHeader name="Júlia" photoUserGithub="juliafclima" />
+      <HomeHeader name="Julia" photoUserGithub="juliafclima" />
 
-      <HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+      />
+
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
         <FlatList
-          data={groups}
+          data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <Group
-              name={item}
-              isActive={groupSelected === item}
-              onPress={() => setGroupSelected(item)}
-            />
-          )}
-          horizontal
-          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
           _contentContainerStyle={{
-            px: 8,
+            paddingBottom: 20,
           }}
-          my={10}
-          maxH={10}
         />
-      </HStack>
+      </VStack>
     </VStack>
   );
 }
