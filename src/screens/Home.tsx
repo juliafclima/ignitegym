bdcd8c2357f452526f82410c004c49cd6366e4ad
Júlia Lima <juliafclima@hotmail.com>
2024-06-27 14:52:21 -0300

    Exibindo o grupo selecionado

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,33 @@
+import { HStack, VStack } from "native-base";
+
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "native-base";
+import { useState } from "react";
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("Costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader name="Júlia" photoUserGithub="juliafclima" />
 
-      <Group name="Costas" />
-      <Group name="Posterior" />
-      <Group name="Pernas" />
+      <HStack>
+        <Group
+          name="Costas"
+          isActive={groupSelected === "costa"}
+          onPress={() => setGroupSelected("costa")}
+        />
+        <Group
+          name="Ombros"
+          isActive={groupSelected === "ombro"}
+          onPress={() => setGroupSelected("ombro")}
+        />
+        <Group
+          name="Pernas"
+          isActive={groupSelected === "perna"}
+          onPress={() => setGroupSelected("perna")}
+        />
+      </HStack>
     </VStack>
   );
 }
