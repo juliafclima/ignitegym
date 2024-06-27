a8a4ae55e91b9b920a522c8aaaee4e19263958ad
Júlia Lima <juliafclima@hotmail.com>
2024-06-27 10:22:14 -0300

    Customizando o tema default

--- App.tsx
+++ App.tsx
@@ -1,29 +1,26 @@
-import { ActivityIndicator, StatusBar, Text, View } from "react-native";
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
+import { StatusBar, Text } from "react-native";
+
+import { Loading } from "@components/Loading";
+import { NativeBaseProvider } from "native-base";
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {fontsLoaded ? <Text>Hello World</Text> : <ActivityIndicator />}
-    </View>
+      {!fontsLoaded ? <Text>Ola, mundo</Text> : <Loading />}
+    </NativeBaseProvider>
   );
 }
