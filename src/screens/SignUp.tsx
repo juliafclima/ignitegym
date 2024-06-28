5b0ab0f0aaa2e673a578b27dfb4ab6a1a62033b1
Júlia Lima <juliafclima@hotmail.com>
2024-06-28 14:43:17 -0300

    Tratando Erros da API

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -17,7 +17,7 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigation } from "@react-navigation/native";
-import axios from "axios";
+import { AppError } from "@utils/AppError";
 import { ImageBackground } from "react-native";
 import { api } from "src/servers/api";
 
@@ -63,13 +63,17 @@ export function SignUp() {
       const response = await api.post("/users", { name, email, password });
       console.log(response.data);
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return toast.show({
-          title: error.response?.data.message,
-          placement: "top",
-          bgColor: "red.500",
-        });
-      }
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
     }
   }
 
