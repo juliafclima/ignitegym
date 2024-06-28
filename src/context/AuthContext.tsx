ceac9d11c662b5fe6f2f39c3b8fb62370b36e199
Júlia Lima <juliafclima@hotmail.com>
2024-06-28 17:07:29 -0300

    Anexando token nas requisições

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,6 +6,7 @@ import {
 import { ReactNode, createContext, useEffect, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
 import { api } from "src/servers/api";
 
 export type AuthContextDataProps = {
@@ -28,13 +29,29 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
     useState(true);
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+
+      setUser(userData);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post("/sessions", { email, password });
 
       if (data.user && data.token) {
-        setUser(data.user);
-        storageUserSave(data.user);
+        storageUserAndToken(data.user, data.token);
       }
     } catch (error) {
       throw error;
